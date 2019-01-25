494bfc3122602ebd3a714f90aa7d420ad16a7513
a54552239 <5455223290@qq.com>
2019-01-25 17:02:31 +0800

    更新版本号至2.1.0

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,4 @@
 export default {
-    VERSION: '2.1.0',
     crossDomain: true, //是否开启跨域支持
     PROD_URL: 'http://127.0.0.1/pearProjectApi/index.php', //生产环境接口地址
     WS_URI: '',//wss://beta.vilson.xyz:2345 //WebSocket地址
---
+++ src/config/version.js
@@ -0,0 +1,3 @@
+export default {
+    VERSION: '2.1.0',
+};
