c725153ae39bb32048f72c9e5cb4f7a56fd6ad27
vilson <545522390@qq.com>
2019-06-16 20:42:56 +0800

    标签列表增加缺省提示

--- src/components/project/taskTag.vue
+++ src/components/project/taskTag.vue
@@ -1,66 +1,73 @@
 <template>
-    <a-tabs class="task-tag-index" tabPosition="left" defaultActiveKey="1" :animated="false" v-model="tabKey">
-        <a-tab-pane :key="taskTag.code" v-for="taskTag in taskTagList">
-            <div slot="tab">
-                <div class="tag-item">
-                    <a-badge status="success"
-                             :class="`badge-${taskTag.color}`"/>
-                    {{taskTag.name}}
+    <div>
+        <a-tabs class="task-tag-index" tabPosition="left" defaultActiveKey="1" :animated="false" v-model="tabKey">
+            <a-tab-pane :key="taskTag.code" v-for="taskTag in taskTagList">
+                <div slot="tab">
+                    <div class="tag-item">
+                        <a-badge status="success"
+                                 :class="`badge-${taskTag.color}`"/>
+                        {{taskTag.name}}
+                    </div>
                 </div>
-            </div>
-            <div class="config-content">
-                <vue-scroll>
-                    <div class="content-item">
-                        <div class="infos">
-                            <a-list :loading="{spinning: loading,delay: 300}">
-                                <div v-if="showLoadingMore" slot="loadMore"
-                                     :style="{ textAlign: 'center', marginTop: '12px', height: '32px', lineHeight: '32px' }">
-                                    <a-spin v-if="loadingMore"/>
-                                    <a-button v-else @click="onLoadMore">加载更多</a-button>
-                                </div>
-                                <a-list-item :key="index" v-for="(item, index) in list">
-                                    <a-list-item-meta>
-                                        <div class="task-list" slot="title" :class="{'done': item.done}">
-                                            <div>
-                                                <!--                                                <a-avatar size="small" icon="user" class="m-r-sm"></a-avatar>-->
+                <div class="config-content">
+                    <vue-scroll>
+                        <div class="content-item">
+                            <div class="infos">
+                                <a-list :loading="{spinning: loading,delay: 300}">
+                                    <div v-if="showLoadingMore" slot="loadMore"
+                                         :style="{ textAlign: 'center', marginTop: '12px', height: '32px', lineHeight: '32px' }">
+                                        <a-spin v-if="loadingMore"/>
+                                        <a-button v-else @click="onLoadMore">加载更多</a-button>
+                                    </div>
+                                    <a-list-item :key="index" v-for="(item, index) in list">
+                                        <a-list-item-meta>
+                                            <div class="task-list" slot="title" :class="{'done': item.done}">
+                                                <div>
+                                                    <!--                                                <a-avatar size="small" icon="user" class="m-r-sm"></a-avatar>-->
 
-                                                <router-link class="text-default task-content"
-                                                             :to="`/project/space/task/${code}/detail/${item.code}`">
-                                                    <a-tooltip placement="top"
-                                                               v-if="item.executor && item.executor.avatar">
-                                                        <template slot="title">
-                                                            <span>{{item.executor.name}}</span>
-                                                        </template>
-                                                        <img
-                                                                :src="item.executor.avatar"
-                                                                :title="item.executor.name"
-                                                                class="avatar task-executor img-circle img-24 hinted m-r-sm">
-                                                    </a-tooltip>
-                                                    <a-avatar size="small" icon="user" class="m-r-sm" v-else></a-avatar>
-                                                    <span class="task-name m-r">{{ item.name }}</span>
-                                                    <span class="muted flex m-r tag-name" v-for="tag in item.tags"
-                                                          :key="tag.code"
-                                                    >
+                                                    <router-link class="text-default task-content"
+                                                                 :to="`/project/space/task/${code}/detail/${item.code}`">
+                                                        <a-tooltip placement="top"
+                                                                   v-if="item.executor && item.executor.avatar">
+                                                            <template slot="title">
+                                                                <span>{{item.executor.name}}</span>
+                                                            </template>
+                                                            <img
+                                                                    :src="item.executor.avatar"
+                                                                    :title="item.executor.name"
+                                                                    class="avatar task-executor img-circle img-24 hinted m-r-sm">
+                                                        </a-tooltip>
+                                                        <a-avatar size="small" icon="user" class="m-r-sm" v-else></a-avatar>
+                                                        <span class="task-name m-r">{{ item.name }}</span>
+                                                        <span class="muted flex m-r tag-name" v-for="tag in item.tags"
+                                                              :key="tag.code"
+                                                        >
                                                     <a-badge status="success"
                                                              :class="`badge-${tag.tag.color} m-r-xs`"/>
                                                    {{tag.tag.name}}
                                                 </span>
-                                                </router-link>
+                                                    </router-link>
+                                                </div>
+                                                <div class="label task-time" :class="showTimeLabel(item.end_time)"
+                                                     v-if="item.end_time">
+                                                    {{showTaskTime(item.begin_time, item.end_time)}}
+                                                </div>
                                             </div>
-                                            <div class="label task-time" :class="showTimeLabel(item.end_time)"
-                                                 v-if="item.end_time">
-                                                {{showTaskTime(item.begin_time, item.end_time)}}
-                                            </div>
-                                        </div>
-                                    </a-list-item-meta>
-                                </a-list-item>
-                            </a-list>
+                                        </a-list-item-meta>
+                                    </a-list-item>
+                                </a-list>
+                            </div>
                         </div>
-                    </div>
-                </vue-scroll>
+                    </vue-scroll>
+                </div>
+            </a-tab-pane>
+            <div class="m-t text-center" v-if="!loading && !list.length">
+                <strong>
+                    <h2>项目中还没有标签</h2>
+                </strong>
             </div>
-        </a-tab-pane>
-    </a-tabs>
+        </a-tabs>
+    </div>
 
 </template>
 
@@ -86,7 +93,7 @@
         data() {
             return {
                 loading: false,
-                tabKey: '1',
+                tabKey: '0',
                 project: {},
                 taskTagList: [],
                 list: [],
@@ -99,7 +106,7 @@
                 this.init();
             },
             tabKey(val) {
-                this.getTaskList(val);
+                this.getTaskList();
             }
 
         },
@@ -116,12 +123,15 @@
                 let app = this;
                 list(app.requestData).then(res => {
                     app.taskTagList = res.data;
-                    if (res.data) {
+                    if (res.data.length) {
                         app.tabKey = res.data[0].code;
                     }
                 });
             },
             getTaskList(reset = true) {
+                if (!this.tabKey) {
+                    return false;
+                }
                 let app = this;
                 app.loading = true;
                 if (reset) {
