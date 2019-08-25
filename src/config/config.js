3517164545a3ffbdc117ba00b0461243b2fdaeaf
vilson <545522390@qq.com>
2019-08-26 06:12:21 +0800

    细节优化

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 export default {
     crossDomain: false, //是否开启跨域支持
-    PROD_URL: 'http://127.0.0.1/pearProjectApi/index.php', //生产环境接口地址
-    WS_URI: '',//wss://beta.vilson.xyz:2345 //WebSocket地址
+    PROD_URL: 'https://beta.vilson.xyz/index.php', //生产环境接口地址
+    WS_URI: 'ws://192.168.31.68:2345',//wss://beta.vilson.xyz:2345 //WebSocket地址
     HOME_PAGE: '/home',//主页路由
 };
