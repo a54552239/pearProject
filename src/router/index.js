666dafb016f5cfc58788b746133f2f490330b992
vilson <545522390@qq.com>
2020-05-09 17:41:15 +0800

    统计

--- src/router/index.js
+++ src/router/index.js
@@ -151,6 +151,11 @@ router.beforeEach((to, from, next) => {
         });
         return false;
     }
+    // 统计代码
+    if (to.path) {
+        _hmt.push(['_trackPageview', '/#' + to.fullPath]);
+        console.log(_hmt);
+    }
     next();
 });
 router.afterEach(route => {
