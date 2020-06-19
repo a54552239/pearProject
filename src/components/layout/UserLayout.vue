3b68e6d581b57a7ccba73fad6ab0b4e3616171eb
vilson <545522390@qq.com>
2020-06-20 07:45:06 +0800

    细节优化

--- src/components/layout/UserLayout.vue
+++ src/components/layout/UserLayout.vue
@@ -17,11 +17,6 @@
             <slot></slot>
 
             <div class="footer">
-                <!--  <div class="links">
-                      <a href="_self">帮助</a>
-                      <a href="_self">隐私</a>
-                      <a href="_self">条款</a>
-                  </div>-->
                 <div class="copyright">
                     Copyright &copy; 2019 Pear Project
                 </div>
