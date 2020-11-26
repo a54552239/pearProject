3f3ed62c94e8c28943f2be262ed4710f5f1fccca
vilson <545522390@qq.com>
2020-11-26 22:57:23 +0800

    细节调整

--- src/components/layout/UserLayout.vue
+++ src/components/layout/UserLayout.vue
@@ -98,7 +98,7 @@
                     .title {
                         font-size: 33px;
                         color: rgba(0, 0, 0, .85);
-                        font-family: "Chinese Quote", -apple-system, BlinkMacSystemFont, "Segoe UI", "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "Helvetica Neue", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+                        //font-family: "Chinese Quote", -apple-system, BlinkMacSystemFont, "Segoe UI", "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "Helvetica Neue", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
                         font-weight: 600;
                         position: relative;
                         top: 2px;
